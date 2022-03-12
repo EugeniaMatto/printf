815571e835d6fba79138086a2f5ca4a51bc1f7cd
Martinliporace <johnd.com>
2022-03-12 10:27:02 -0800

    funciones auxiliares fixed

--- func_aux.c
+++ func_aux.c
@@ -14,20 +14,3 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-
-/**
-* _strlen - devuelve el largo de la cadena recibida
-* @s: cadena recibida
-* Return: largo de la cadena
-*/
-
-int _strlen(char *s)
-
-{
-int c = 0;
-while (s[c] != '\0')
-{
-c++;
-}
-return (c);
-}
