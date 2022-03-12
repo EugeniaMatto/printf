eb918199ee68f2c03ea1d72acd861bdf486d216d
EugeniaMatto <eugematto26@gmail.com>
2022-03-12 09:40:06 -0800

    add main.h fixed

--- main.h
+++ main.h
@@ -1,21 +1,24 @@
 #ifndef _PRINTF_
 #define _PRINTF_
-#define NULL ((char *)0)
+#define NULL ((char *)0) 
 #include <stdarg.h>
 
 /**
  * struct print_ - Correspondera la letra con la funcion para printear
  *
  * @l: letra que nos pasaran en el format, despues de un %
- * @f: la funcion que corresponde a la letra, ej c -> print_char
+ * @f: la funcion
  */
 typedef struct print_
 {
 	char *l;
-	int (*f)(va_list);
+	int (*f)(va_list)
 } print;
 
-int _putchar(char);
+int _putchar(va_list);
+int _strlen(va_list);
+int print_number(va_list);
+
 int _printf(const char *format, ...);
 
 
