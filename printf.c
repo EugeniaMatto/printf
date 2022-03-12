88d8bae828e074879b4b004a527b64ac6ffd6047
EugeniaMatto <eugematto26@gmail.com>
2022-03-12 08:56:05 -0800

    add printf function

---
+++ printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: num args
+ * @argv: args
+ * Return: Always 0.
+ */
+int _printf(const char *format, ...)
+{
+	int b, i = 0;
+	int cont;
+	va_list lista;
+
+	print print_s[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_int},
+		{NULL, NULL}
+	};
+
+	va_start(lista, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			b = 0;
+			while (print_s[b].l != NULL)
+			{
+				if (print_s[b].l[0] == format[i + 1])
+				{
+					cont += print_s[b].f(lista);
+				}
+			b++;
+			}
+		}
+		else
+		{
+		_putchar(format[i]);
+		cont++;
+		}
+	i++;
+	}
+	
+	return(cont);
+}
