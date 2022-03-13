648c380b757c6a2ca974c6c7ddbef0b7648cfa45
EugeniaMatto <eugematto26@gmail.com>
2022-03-12 16:17:03 -0800

    add function pertenece

--- func_aux.c
+++ func_aux.c
@@ -6,11 +6,29 @@
  * @c: carcater a imprimir
  *
  * Return: 1
- * 
+ *
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
+/**
+ * pertenece - identifica si el char es una de las letras admitidas
+ * @c: letra de format, siguiente a un %
+ * Return: 1 si pertenece, 0 caso contrario
+ */
+int pertenece(char c)
+{
+	int i = 0;
+	char *arr = "csdibuoxXSp";
+
+	while (arr[i])
+	{
+		if (arr[i] == c)
+			return (1);
+	i++;
+	}
+
+	return (0);
+}
