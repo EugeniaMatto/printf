e046dba20f34ff7534a095ac1d9d67d72ffa374f
Martinliporace <johnd.com>
2022-03-14 07:43:28 -0700

    added print_non_print

--- funciones_advanced2.c
+++ funciones_advanced2.c
@@ -90,4 +90,54 @@ int print_rot13(va_list lista)
 	return (cont);
 }
 
+/**
+ * print_non_print - imprime la cadena reemplazando caracteres
+ * no imprimibles por '\x + su valor ASCII'
+ * @lista: argumentos
+ *
+ * Return: 0
+ */
+int print_non_print(va_list lista)
 
+{
+	char *s;
+	int resto, num_d, i , cont = 0;
+	int n_h[100];
+
+	s = va_arg(lista, char*);
+	if (!s)
+		s = "(null)";
+	while (s[cont])
+	{
+		if ((s[cont] > 0 && s[cont] < 32) || (s[cont] >= 127))
+		{
+			num_d = (int)s[cont];
+			while (num_d != 0)
+			{
+				resto = num_d % 16;
+				if (resto < 10)
+					resto = resto + 48;
+				else
+					resto = resto + 55;
+				n_h[i] = resto;
+				i++;
+				num_d = num_d / 16;
+			}
+			_putchar('\\');
+			_putchar('x');
+			i = i - 1;
+			if (i < 1)
+				_putchar('0');
+			for (; i >= 0; i--)
+			{
+				_putchar(n_h[i]);
+			}
+			i = 0;
+			
+		}
+		else
+			_putchar(s[cont]);
+	cont++;
+	}
+	return (cont);
+}
