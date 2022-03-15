58dec6fc5e96f06b28c821d5867b645318030ba3
EugeniaMatto <eugematto26@gmail.com>
2022-03-14 17:13:08 -0700

    fix print_hexadecimal

--- funciones_advanced.c
+++ funciones_advanced.c
@@ -3,14 +3,18 @@
  * print_hexadecimal - imprime en hexadecimal
  * @lista: argumentos
  * @tipo: 3 si imprime adress,2 si impr. hexa en minus,1 si imp. hexa en MAYUS
- *
  * Return: cantidad de caracteres impresos
  */
 int print_hexadecimal(va_list lista, int tipo)
 {
 	int i = 0, cont = 0, resto, suma = 87;
 	char n_h[100];
-	unsigned long num_d = (unsigned long) va_arg(lista, unsigned long);
+	unsigned long num_d;
+
+	if (tipo == 3)
+		num_d = (unsigned long) va_arg(lista, unsigned long);
+	else
+		num_d = (unsigned long) va_arg(lista, unsigned int);
 
 	if (tipo == 3 && num_d > 0)
 		cont += print_base();
@@ -23,14 +27,12 @@ int print_hexadecimal(va_list lista, int tipo)
 		_putchar('0');
 		return (1);
 	}
-
 	if (tipo == 1)
 		suma = 55;
 
 	while (num_d != 0)
 	{
 		resto = num_d % 16;
-
 		if (resto < 10)
 			resto = resto + 48;
 		else
@@ -40,7 +42,6 @@ int print_hexadecimal(va_list lista, int tipo)
 		i++;
 		num_d = num_d / 16;
 	}
-
 	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(n_h[i]);
